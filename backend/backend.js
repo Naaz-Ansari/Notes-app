444ec6c8cdd0e352bfa5d6fc0d59344c22335263
Naaz-Ansari <naazansari172001@gmail.com>
2025-01-27 21:18:09 +0530

    updated

--- backend/backend.js
+++ backend/backend.js
@@ -17,7 +17,7 @@ app.use(cors());
 const _dirname = path.resolve();
 
 //Conneting to database
-mongoose.connect('mongodb+srv://naazansari172001:uKGKuH1O7KgXpd00@notes-app.a2wj4.mongodb.net/?retryWrites=true&w=majority&appName=Notes-App', {
+mongoose.connect('mongodb+srv://naazansari172001:uKGKuH1O7KgXpd00@notes-app.a2wj4.mongodb.net/?retryWrites=true&w=majority&appName=Notes-App/test', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4,
@@ -28,7 +28,7 @@ mongoose.connect('mongodb+srv://naazansari172001:uKGKuH1O7KgXpd00@notes-app.a2wj
 })
 
 //Mongodb Client connection
-const uri = "mongodb+srv://naazansari172001:uKGKuH1O7KgXpd00@notes-app.a2wj4.mongodb.net/?retryWrites=true&w=majority&appName=Notes-App";
+const uri = "mongodb+srv://naazansari172001:uKGKuH1O7KgXpd00@notes-app.a2wj4.mongodb.net/?retryWrites=true&w=majority&appName=Notes-App/test";
 // const client = new MongoClient(uri);
 
 const client = new MongoClient(uri, {
